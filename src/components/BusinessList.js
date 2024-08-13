a6a688dcf2d6d3163b736794acb89dcbd9eb6e73
pillllo <natalie.s.pilling@gmail.com>
2024-08-13 21:09:53 +0200

    Fix indexOf issue

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -6,7 +6,7 @@ const businesses = [1, 2, 3, 4];
 function BusinessList() {
     return (
         <>
-            {businesses.map(business => <Business key={indexOf(business)}/>)}
+            {businesses.map(business => <Business key={businesses.indexOf(business)}/>)}
         </>
     )
 };
